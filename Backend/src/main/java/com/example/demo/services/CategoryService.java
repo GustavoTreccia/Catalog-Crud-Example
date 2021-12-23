feddb10c13abd01c2449a66f0ec8fc9ce60cb4ad
GustavoTreccia <g.treccia@outlook.com>
2021-12-23 15:36:24 -0300

    Second backup commit

--- Backend/src/main/java/com/example/demo/services/CategoryService.java
+++ Backend/src/main/java/com/example/demo/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.CategoryDTO;
 import com.example.demo.entities.Category;
 import com.example.demo.repositories.CategoryRepository;
 
@@ -13,8 +16,10 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
-		return repository.findAll();
+
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
---
+++ Backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category (name) VALUES ('NADINHA1');
+INSERT INTO tb_category (name) VALUES ('NADINHA2');
+INSERT INTO tb_category (name) VALUES ('NADINHA3');
